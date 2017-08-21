d96a6691c86f324e20dc3a56bb4912c6d3b8a4a4
jamie-jessi <jamiejudi@gmail.com>
2017-08-20 22:43:11 -0500

    connection

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql'); //mysql connnection + config
+
+var connection = mysql.connection({
+    port: 3000,
+    host: "localhost",
+    user: "root",
+    password: "MEclass@2003",
+    database: "burgers_db"
+});
+
+connection.connect(function(err) { //connection
+    if (err) {
+        console.error("error: " + err.stack);
+        return;
+    }
+    console.log("connected as " + connection.threadId);
+});
+
+module.exports = connection //exports connection

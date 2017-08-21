8c06cbaeb54919d2936d1d446d27e18ae409753e
jamie-jessi <jamiejudi@gmail.com>
2017-08-21 01:13:22 -0500

    rename

--- models/burger.js
+++ models/burger.js
@@ -1,18 +1,18 @@
 var orm = require("../config/orm.js"); //imports orm to create functions that will interact with databse
 
 var burger = { //orm calls
-  selectAll: function(cb) {
+  all: function(cb) {
     orm.all("burgers", function(res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
-  insertOne: function(cols, vals, cb) {
+  create: function(cols, vals, cb) {
     orm.create("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
-  updateOne: function(objColVals, condition, cb) {
+  update: function(objColVals, condition, cb) {
     orm.update("burgers", objColVals, condition, function(res) {
       cb(res);
     });

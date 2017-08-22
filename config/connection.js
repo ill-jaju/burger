4a806efaf0ada40d07f2ad84c58c082fc7408244
jamie-jessi <jamiejudi@gmail.com>
2017-08-22 00:03:00 -0500

    jaws

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 var mysql = require('mysql'); //mysql connnection + config
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "MEclass@2003",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "MEclass@2003",
+        database: "burgers_db"
+    });
+};
 
-connection.connect(function(err) { //connection
+connection.connect(function (err) { //connection
     if (err) {
         console.error("error: " + err.stack);
         return;
